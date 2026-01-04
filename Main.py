cf20cc46fa785fd96aeabdabff3e4dfca66b60f6
selin duru derindag <derindagsd@gmail.com>
2026-01-04 17:41:32 +0300

    changed image type from jpg to png

--- Main.py
+++ Main.py
@@ -2,5 +2,5 @@ import Processor
 
 if __name__ == '__main__':
     for i in range (1,4):
-        img_path = "./data/" + str(i) + ".jpg"
+        img_path = "./data/" + str(i) + ".png"
         Processor.execute(img_path)
